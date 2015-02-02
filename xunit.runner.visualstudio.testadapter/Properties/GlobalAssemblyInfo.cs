26c334ee8b2c8c0b9f783db63478fae70e612208
seabiscuit <hmkhan@qq.com>
2015-02-02 22:03:51 +0800

    移除xunit.runner.visualstudio.testadapter自编译， 采用官方最新版本，xunit.runner.visualstudio.2.0.0-rc1-build1035

--- xunit.runner.visualstudio.testadapter/Properties/GlobalAssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyCompany("Outercurve Foundation")]
-[assembly: AssemblyProduct("xUnit.net Runner for Visual Studio")]
-[assembly: AssemblyCopyright("Copyright (C) Outercurve Foundation")]
-[assembly: AssemblyVersion("2.0.0.0")]
