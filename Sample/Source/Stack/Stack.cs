685cc06e33096a60e43f2d9a33a686ffd186990a
seabiscuit <hmkhan@qq.com>
2015-02-03 22:04:22 +0800

    Stack

--- Sample/Source/Stack/Stack.cs
+++ Sample/Source/Stack/Stack.cs
@@ -2,37 +2,44 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-public class Stack<T>
+#if NET40
+namespace Stack40
+#else
+namespace Stack45
+#endif
 {
-    List<T> elements = new List<T>();
+	public class Stack<T>
+	{
+		List<T> elements = new List<T>();
 
-    public int Count
-    {
-        get { return elements.Count; }
-    }
+		public int Count
+		{
+			get { return elements.Count; }
+		}
 
-    public bool Contains(T element)
-    {
-        return elements.Contains(element);
-    }
+		public bool Contains(T element)
+		{
+			return elements.Contains(element);
+		}
 
-    public T Peek()
-    {
-        if (Count == 0)
-            throw new InvalidOperationException("empty stack");
+		public T Peek()
+		{
+			if (Count == 0)
+				throw new InvalidOperationException("empty stack");
 
-        return elements.Last();
-    }
+			return elements.Last();
+		}
 
-    public T Pop()
-    {
-        T element = Peek();
-        elements.RemoveAt(Count - 1);
-        return element;
-    }
+		public T Pop()
+		{
+			T element = Peek();
+			elements.RemoveAt(Count - 1);
+			return element;
+		}
 
-    public void Push(T element)
-    {
-        elements.Add(element);
-    }
+		public void Push(T element)
+		{
+			elements.Add(element);
+		}
+	}
 }
