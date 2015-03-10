4f966a0b09c96d4b1a62f9cd915d2fc3d5a5a4b9
seabiscuit <hmkhan@qq.com>
2015-03-10 15:42:43 +0800

    CategoryAttribute => xunit.extensions

---
+++ xunit.extensions.net45/Trait/CategoryAttribute.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit.Sdk;
+
+namespace Xunit
+{
+	/// <summary>Apply this attribute to your test method to specify a category.</summary>
+	[TraitDiscoverer("Xunit.Sdk.CategoryDiscoverer", "xunit.extensions")]
+	[AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+	public class CategoryAttribute : Attribute, ITraitAttribute
+	{
+		public CategoryAttribute(String category) { }
+	}
+}
---
+++ xunit.extensions.net45/Trait/CategoryDiscoverer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit.Abstractions;
+
+namespace Xunit.Sdk
+{
+	/// <summary>This class discovers all of the tests and test classes that have applied the Category attribute</summary>
+	public class CategoryDiscoverer : ITraitDiscoverer
+	{
+		/// <summary>Gets the trait values from the Category attribute.</summary>
+		/// <param name="traitAttribute">The trait attribute containing the trait values.</param>
+		/// <returns>The trait values.</returns>
+		public IEnumerable<KeyValuePair<String, String>> GetTraits(IAttributeInfo traitAttribute)
+		{
+			var ctorArgs = traitAttribute.GetConstructorArguments().ToList();
+			yield return new KeyValuePair<String, String>("Category", ctorArgs[0].ToString());
+		}
+	}
+}
